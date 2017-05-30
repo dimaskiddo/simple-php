04ccc223e3ae0b67a55555faa1025905c7b3343a
Dimas Restu H <dimas.restu@student.upi.edu>
2017-05-30 15:52:09 +0700

    Update DBTest Success Information & Environment

--- dbtest.php
+++ dbtest.php
@@ -6,13 +6,20 @@
   $dbname = getenv("MYSQL_DATABASE");
   
   $dbconn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-  if (!dbconn) {
+  if (!dbconn)
+  {
     echo "Error Connetion Database !<br/>\n";
-    echo "Environment:<br/>\n";
-    echo "- Host: ".$dbhost."<br/>\n";
-    echo "- Port: ".$dbport."<br/>\n";
-    echo "- User: ".$dbuser."<br/>\n";
-    echo "- Pass: ".$dbpass."<br/>\n";
-    echo "- Name: ".$dbname."<br/>\n";
   }
+  else
+  {
+    echo "Connection Success !<br/>\n";
+  }
+
+  echo "Environment:<br/>\n";
+  echo "- Host: ".$dbhost."<br/>\n";
+  echo "- Port: ".$dbport."<br/>\n";
+  echo "- User: ".$dbuser."<br/>\n";
+  echo "- Pass: ".$dbpass."<br/>\n";
+  echo "- Name: ".$dbname."<br/>\n";
+
 ?>
